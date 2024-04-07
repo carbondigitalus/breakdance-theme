dfc7d8bfc9d8d3389663f349548a0600c14b987d
Jared Ledbetter <jared.ledbetter@outlook.com>
2024-04-06 23:08:34 -0400

    update functions php with BD custom element plugin

--- functions.php
+++ functions.php
@@ -1,17 +1,30 @@
 <?php
 
-/**
- * Plugin Name: Breakdance Custom Elements
- * Plugin URI: https://breakdance.com/
- * Description: Boilerplate plugin to save your custom elements created with Element Studio.
- * Author: Breakdance
- * Author URI: https://breakdance.com/
- * License: GPLv2
- * Text Domain: breakdance
- * Domain Path: /languages/
- * Version: 0.0.1
- */
+// Theme setup function
+if (!function_exists('breakdance_zero_theme_setup')) {
+    function breakdance_zero_theme_setup()
+    {
+        add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+    }
+}
 
+add_action('after_setup_theme', 'breakdance_zero_theme_setup');
+
+// Admin notice if Breakdance is disabled
+add_action('admin_notices', 'warn_if_breakdance_is_disabled');
+function warn_if_breakdance_is_disabled()
+{
+    if (!defined('__BREAKDANCE_DIR__')) {
+        ?>
+        <div class="notice notice-error is-dismissible">
+            <p>You're using Breakdance's Zero Theme but Breakdance is not enabled. This isn't supported.</p>
+        </div>
+        <?php
+    }
+}
+
+// Breakdance Custom Elements
 namespace BreakdanceCustomElements;
 
 use function Breakdance\Util\getDirectoryPathRelativeToPluginFolder;
@@ -30,7 +43,7 @@ add_action('breakdance_loaded', function () {
         'BreakdanceCustomElements',
         'macro',
         'Custom Macros',
-        false,
+        false
     );
 
     \Breakdance\ElementStudio\registerSaveLocation(
@@ -38,36 +51,7 @@ add_action('breakdance_loaded', function () {
         'BreakdanceCustomElements',
         'preset',
         'Custom Presets',
-        false,
+        false
     );
-},
-    // register elements before loading them
-    9
-);
+}, 9); // Register elements before loading them
 
-if (!function_exists('breakdance_zero_theme_setup')) {
-    function breakdance_zero_theme_setup()
-    {
-        add_theme_support('title-tag');
-        add_theme_support( 'post-thumbnails' );
-
-    }
-}
-
-add_action('after_setup_theme', 'breakdance_zero_theme_setup');
-
-
-add_action( 'admin_notices', 'warn_if_breakdance_is_disabled' );
-
-function warn_if_breakdance_is_disabled() {
-    if (defined('__BREAKDANCE_DIR__')){
-        return;
-    }
-
-    ?>
-    <div class="notice notice-error is-dismissible">
-        <p>You're using Breakdance's Zero Theme but Breakdance is not enabled. This isn't supported.</p>
-    </div>
-    <?php
-}
-?>
