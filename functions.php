9e99be1ced7474647799f7841c5c2e55e89e2fda
Jared Ledbetter <jared.ledbetter@outlook.com>
2024-04-06 22:24:50 -0400

    add custom elements plugin to functions php file

--- functions.php
+++ functions.php
@@ -1,5 +1,50 @@
 <?php
 
+/**
+ * Plugin Name: Breakdance Custom Elements
+ * Plugin URI: https://breakdance.com/
+ * Description: Boilerplate plugin to save your custom elements created with Element Studio.
+ * Author: Breakdance
+ * Author URI: https://breakdance.com/
+ * License: GPLv2
+ * Text Domain: breakdance
+ * Domain Path: /languages/
+ * Version: 0.0.1
+ */
+
+namespace BreakdanceCustomElements;
+
+use function Breakdance\Util\getDirectoryPathRelativeToPluginFolder;
+
+add_action('breakdance_loaded', function () {
+    \Breakdance\ElementStudio\registerSaveLocation(
+        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/elements',
+        'BreakdanceCustomElements',
+        'element',
+        'Custom Elements',
+        false
+    );
+
+    \Breakdance\ElementStudio\registerSaveLocation(
+        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/macros',
+        'BreakdanceCustomElements',
+        'macro',
+        'Custom Macros',
+        false,
+    );
+
+    \Breakdance\ElementStudio\registerSaveLocation(
+        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/presets',
+        'BreakdanceCustomElements',
+        'preset',
+        'Custom Presets',
+        false,
+    );
+},
+    // register elements before loading them
+    9
+);
+
 if (!function_exists('breakdance_zero_theme_setup')) {
     function breakdance_zero_theme_setup()
     {
@@ -25,3 +70,4 @@ function warn_if_breakdance_is_disabled() {
     </div>
     <?php
 }
+?>
