d4f353b1e1692b675d0f51e80ca1fea1b81026bb
Jared Ledbetter <57743183+jared-leddy@users.noreply.github.com>
2026-08-20 23:26:29 -0400

    add gutenberg styling

--- functions.php
+++ functions.php
@@ -4,15 +4,20 @@ if (!function_exists('breakdance_zero_theme_setup')) {
     function breakdance_zero_theme_setup()
     {
         add_theme_support('title-tag');
-        add_theme_support( 'post-thumbnails' );
+        add_theme_support('post-thumbnails');
 
+        // Enable custom Gutenberg editor styles.
+        add_theme_support('editor-styles');
+
+        // Load editor-specific CSS.
+        add_editor_style('editor-style.css');
     }
 }
 
 add_action('after_setup_theme', 'breakdance_zero_theme_setup');
 
 
-add_action( 'admin_notices', 'warn_if_breakdance_is_disabled' );
+add_action('admin_notices', 'warn_if_breakdance_is_disabled');
 
 function warn_if_breakdance_is_disabled() {
     if (defined('__BREAKDANCE_DIR__')){
